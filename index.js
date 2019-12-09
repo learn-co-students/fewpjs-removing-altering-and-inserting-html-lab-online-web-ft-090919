a2984f47f95fbc8048ad640a982764c70212ccc8
mmman64 <mcnultymarco@gmail.com>
2019-12-09 21:26:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Marco is the champion";
+
+document.body.append(newHeader);
