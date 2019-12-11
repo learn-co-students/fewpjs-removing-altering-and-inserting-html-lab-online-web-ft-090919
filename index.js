d1e74d40fef1359a8ccf0518dfa07f872223e8a3
Doug Kintop <dkintop@hotmail.com>
2019-12-11 12:56:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 let e = document.getElementById("main");
 e.remove();
 
-let newHeader = document.createElement("h1#victory")("h1#victory");
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
 document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "Doug is the champion";
+// const testVar = "hi";
+
+// /* Enter the code to remove the node element here */
+// document.querySelector("main#main").remove();
+
+// /* Create your new element here and assign it to newHeader */
+// const newHeader = document.createElement("h1");
+// newHeader.id = "victory";
+// newHeader.innerHTML = "Flatiron School is the champion!";
+// newHeader.className = "victory";
