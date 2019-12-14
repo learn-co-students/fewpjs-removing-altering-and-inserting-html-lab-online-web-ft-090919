cad4a06cad4c29e93f26e95d5da95e381379e89f
Rsja12 <asocarras10@gmail.com>
2019-12-13 21:48:24 -0500

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ let main = document.querySelector('main')
 main.remove()
 
 let newHeader = document.createElement('h1')
-newHeader.setAttribute('id', 'victory')
+// newHeader.setAttribute('id', 'victory')
+newHeader.id = 'victory'
 newHeader.innerHTML = "A is the champion"
