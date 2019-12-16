e7c4cc8a3387ba89fda745dede7424a18f881d4f
DBuckley0126 <dannyboy.msn@hotmail.com>
2019-12-16 19:45:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.getElementById("main").remove()
+const newHeader = document.getElementsByTagName("body")[0].appendChild(document.createElement("h1"))
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "DANNY is the champion"
