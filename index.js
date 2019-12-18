1b068aa6e86dc5ab5ae918abc294c59e3e77a23e
Jendnice <Pipeline38@hotmail.com>
2019-12-18 13:52:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Write your code here!
+// let element = document.createElement('div')
+
+// document.body.appendChild(element)
+
+// let ul = document.createElement('ul')
+
+// for (let i = 0; i < 3; i++) {
+//     let li = document.createElement('li')
+//     li.innerHTML = (i + 1).toString()
+//     ul.appendChild(li)
+// }
+
+// element.appendChild(ul)
+
+
+
+// // let element = document.querySelector("p#greeting");
+// // element.innerHTML = "Hello, DOM!"
+
+
+
+// // let header = document.getElementById("div#header");
+// // header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+
+
+// element.style.backgroundColor = '#27647B';
+
+// element.style.textAlign = 'center';
+// ul.style.textAlign = 'left'
+
+// ul.removeChild(ul.querySelector('li:nth-child(2)'))
+
+// // ul.remove()
+
+
+
+let main = document.querySelector("main#main");
+
+main.remove()
+
+
+let newHeader = document.createElement('h1')
+// document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.innerHTML = "<h1>Jen is the champion</h1>"
+
+
+
+
