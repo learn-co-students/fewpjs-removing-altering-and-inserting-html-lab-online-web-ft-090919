2a02b3a51c7e0531292367996cf24a94fde6223d
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-01-09 12:05:23 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+let newHeader = document.createElement('header')
+
+document.body.appendChild(newHeader)
+
+// let h1 = document.createElement('h1')
+
+for (let i = 0; i < 3; i++) {
+  let h1 = document.createElement('h1')
+  h1.innerHTML = "Sarah is the champion".toString()
+  newHeader.appendChild(h1)
+  
+}
