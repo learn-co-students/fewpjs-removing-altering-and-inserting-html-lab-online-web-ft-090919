42e6778c8cbb4830f73234532e9b3210d8f9b70a
yasmine-hj <yasminehajaji95@gmail.com>
2020-01-20 10:31:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Write your code here!
 let main = document.querySelector("main#main");
-
 main.remove()
-
-
 let newHeader = document.createElement('h1')
 document.body.appendChild(newHeader)
 newHeader.id = 'victory'
